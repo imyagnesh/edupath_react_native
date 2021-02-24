2fb553b9b06f8a7e9e5e3986b8d2bc9f52dc1e10
Mohammad Sarosh <mohammadsarosh.siddiqui@hsc.com>
2021-02-24 17:39:21 +0530

    task - button highlight

--- src/components/EPButton/index.js
+++ src/components/EPButton/index.js
@@ -3,9 +3,10 @@ import {Pressable} from 'react-native';
 import EPText from '../EPText';
 import styles from '../../../commonStyle';
 
-const EPButton = ({children, btnStyle, textStyle, textProps, ...props}) => {
+const EPButton = ({children, btnStyle, textStyle, textProps, highlighted, ...props}) => {
+  const bgColor = highlighted ? { backgroundColor: 'darkblue' } : { backgroundColor: 'blue' };
   return (
-    <Pressable style={[styles.btn, styles.center, btnStyle]} {...props}>
+    <Pressable style={[styles.btn, styles.center, btnStyle, bgColor]} {...props}>
       <EPText
         variant="body1"
         style={[styles.textWhite, styles.textCenter, textStyle]}
